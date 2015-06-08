44def047bcf56e0b2e05c97e0ae037c46af62bdc
Alicia MARIN <ma202240@etu.unice.fr>
2015-06-08 10:34:07 +0200

    Ajout des 3 strategies à remplir

---
+++ FIFO_strategy.c
@@ -0,0 +1,46 @@
+#include <assert.h>
+
+#include "strategy.h"
+#include "low_cache.h"
+#include "random.h"
+#include "time.h"
+
+/*!
+ * RAND : pas grand chose à faire ici. 
+ *
+ * En fait, nous initialisons le germe
+ * (seed) du générateur aléatoire à quelque chose d'éminemment variable, pour
+ * éviter d'avoir la même séquence à chque exécution...
+ */
+void *Strategy_Create(struct Cache *pcache) {
+}
+
+void Strategy_Close(struct Cache *pcache)
+{
+}
+
+
+void Strategy_Invalidate(struct Cache *pcache)
+{
+}
+
+
+struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
+{
+    return &pcache->headers[ib];
+}
+
+
+
+void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
+{
+}  
+
+void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
+{
+} 
+
+char *Strategy_Name()
+{
+    return "FIFO";
+}
