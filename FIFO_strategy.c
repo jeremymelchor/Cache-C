e311be755c28a77cd8b9f4777998122d066aa4b4
Alicia MARIN <ma202240@etu.unice.fr>
2015-06-08 14:30:49 +0200

    Strategie fifo avec des problemes corrigés

--- FIFO_strategy.c
+++ FIFO_strategy.c
@@ -2,55 +2,51 @@
 
 #include "strategy.h"
 #include "low_cache.h"
-#include "random.h"
-#include "time.h"
+#include "cache_list.h"
 
 #define C_LIST(pcache) ((struct Cache_List *)((pcache)->pstrategy))
 
- void *Strategy_Create(struct Cache *pcache) 
- {
-     return Cache_List_Create();
- }
+void *Strategy_Create(struct Cache *pcache) 
+{
+    return Cache_List_Create();
+}
 
- void Strategy_Close(struct Cache *pcache)
- {
-     Cache_List_Delete(C_LIST(pcache));
- }
+void Strategy_Close(struct Cache *pcache)
+{
+    Cache_List_Delete(C_LIST(pcache));
+}
 
- void Strategy_Invalidate(struct Cache *pcache)
- {
-     Cache_List_Clear(C_LIST(pcache));
- }
-
+void Strategy_Invalidate(struct Cache *pcache)
+{
+    Cache_List_Clear(C_LIST(pcache));
+}
 
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
-	struct Cache_Block_Header *pbh;
+    struct Cache_Block_Header *pbh;
     struct Cache_List *c_list = C_LIST(pcache);
 
-	/* S'il existe un cache invalide, on va utiliser celui la */
-   if ((pbh = Get_Free_Block(pcache)) != NULL)
-   {
-       // Comme on va l'utiliser, on le met en fin de liste
-       Cache_List_Append(c_list, pbh);
-       return pbh;
-   }
+    /* S'il existe un cache invalide, on va utiliser celui la */
+    if ((pbh = Get_Free_Block(pcache)) != NULL)
+    {
+    	// Comme on va l'utiliser, on le met en fin de liste
+        Cache_List_Append(c_list, pbh);
+        return pbh;
+    }
 
-   // On prend le premier de la liste que l'on va retourner
-   pbh = Cache_List_Remove_First(c_list);
+    // On prend le premier de la liste que l'on va retourner
+    pbh = Cache_List_Remove_First(c_list);
 
-   // Comme on va l'utiliser, on le met en fin de liste
-   Cache_List_Append(c_list, pbh);
+    // Comme on va l'utiliser, on le met en fin de liste
+    Cache_List_Append(c_list, pbh);
 
-   return pbh;
+    return pbh;    
 }
-
-
 
 void Strategy_Read(struct Cache *pcache, struct Cache_Block_Header *pbh) 
 {
 }  
-
+  
 void Strategy_Write(struct Cache *pcache, struct Cache_Block_Header *pbh)
 {
 } 
