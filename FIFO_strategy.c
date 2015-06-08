4a9d2fd3a9648e01830ab922be00a82375c72fce
Alicia MARIN <ma202240@etu.unice.fr>
2015-06-08 14:03:56 +0200

    Strategie fifo finie

--- FIFO_strategy.c
+++ FIFO_strategy.c
@@ -5,7 +5,7 @@
 #include "random.h"
 #include "time.h"
 
-#define FIFO_LIST(pcache)
+#define C_LIST(pcache) ((struct Cache_List *)((pcache)->pstrategy))
 
  void *Strategy_Create(struct Cache *pcache) 
  {
@@ -14,18 +14,35 @@
 
  void Strategy_Close(struct Cache *pcache)
  {
-     Cache_List_Delete(FIFO_LIST(pcache));
+     Cache_List_Delete(C_list(pcache));
  }
 
  void Strategy_Invalidate(struct Cache *pcache)
  {
-     Cache_List_Clear(FIFO_LIST(pcache));
+     Cache_List_Clear(C_list(pcache));
  }
 
 
 struct Cache_Block_Header *Strategy_Replace_Block(struct Cache *pcache) 
 {
-    return ;
+	struct Cache_Block_Header *pbh;
+    struct Cache_List *c_list = C_LIST(pcache);
+
+	/* S'il existe un cache invalide, on va utiliser celui la */
+   if ((pbh = Get_Free_Block(pcache)) != NULL)
+   {
+       // Comme on va l'utiliser, on le met en fin de liste
+       Cache_List_Append(fifo_list, pbh);
+       return pbh;
+   }
+
+   // On prend le premier de la liste que l'on va retourner
+   pbh = Cache_List_Remove_First(fifo_list);
+
+   // Comme on va l'utiliser, on le met en fin de liste
+   Cache_List_Append(fifo_list, pbh);
+
+   return pbh;
 }
 
 
