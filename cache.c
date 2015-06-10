d48a6a7d69b16ca363b66300fc14e9e1cc053b14
MEURGUES Nicolas <meurgues.nicolas@etu.unice.fr>
2015-06-10 11:46:50 +0200

    Correction de toutes les erreurs de compilation

--- cache.c
+++ cache.c
@@ -13,16 +13,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include "low_cache.h"
+#include "cache_list.h"
+#include "random.h"
+#include "strategy.h"
 #include "cache.h"
 
 struct Cache *Cache_Create(const char *fic, unsigned nblocks, unsigned nrecords,
                            size_t recordsz, unsigned nderef) {
 
-	struct Cache *cache = malloc(sizeof(Cache));
+	struct Cache *cache = malloc(sizeof(struct Cache));
+	char * name = fic;
 
-	fopen(fic, NULL);
-	cache->file = fic;
+	FILE *fp = fopen(fic, "rw");
+	cache->file = name;
+	cache->fp = fp;
 	cache->nblocks = nblocks;
 	cache->nrecords = nrecords;
 	cache->recordsz = recordsz;
@@ -49,7 +57,7 @@ Cache_Error Cache_Close(struct Cache *pcache) {
 	Cache_Error c_err;
 	Cache_Sync(pcache);
 	pcache->instrument.n_syncs++;
-	if (fclose(pcache->file) != 0) {
+	if (fclose(pcache->fp) != 0) {
 		c_err = CACHE_KO;
 		return c_err;
 	}
@@ -68,17 +76,20 @@ Cache_Error Cache_Sync(struct Cache *pcache) {
 	//Réccupèration du premier Cache_Block_Header
 	struct Cache_Block_Header *cur_header = pcache->headers;
 	int tmp = 0;
+	int address = &cur_header;
 	//On parcourt la liste de Cache_Block_Header
 	while( tmp < pcache->nblocks) {
+		struct Cache_Block_Header *ad = (struct Cache_Block_Header *)address;
 		//Si on a effectué un multiple de NSYNC accès au Cache, on lance Cache_Sync()
 		if (tmp%(NSYNC) == 0) {
 			Cache_Sync(pcache);
 			pcache->instrument.n_syncs++;
 		}
 		//Si le bit M vaut 1, on écrit le bloc dans le fichier, puis on remet M à 0
-		if (cur_header->flags == 1 || cur_header->flags == 3 || cur_header->flags == 5 || cur_header->flags == 7) {
+		if (ad->flags <= 7 && ad->flags%2 == 1) {
 			//Ecriture dans le fichier
-			if (write(pcache->file, cur_header->data, sizeof(cur_header->data))<0) {
+			int fd = open(pcache->file, O_WRONLY);
+			if (write(fd, ad->data, sizeof(cur_header->data))<0) {
 				c_err = CACHE_KO;
 				return c_err;
 			}
@@ -86,7 +97,7 @@ Cache_Error Cache_Sync(struct Cache *pcache) {
 			cur_header->flags -= 1;
 		}
 		//On change de Cache_Block_Header puis on incrémente le nombre de Cache_Block_Header visité
-		&cur_header += sizeof(struct Cache_Block_Header);
+		address = address + sizeof(struct Cache_Block_Header);
 		tmp++;
 	}
 	//On retourne le Cache_Error
@@ -101,14 +112,16 @@ Cache_Error Cache_Invalidate(struct Cache *pcache){
 	//Réccupèration du premier Cache_Block_Header
 	struct Cache_Block_Header *cur_header = pcache->headers;
 	int tmp = 0;
+	int address = &cur_header;
 	//On parcourt la liste de Cache_Block_Header
 	while( tmp < pcache->nblocks) {
+		struct Cache_Block_Header *ad = (struct Cache_Block_Header *)address;
 		//Si le bit V vaut 1 on le remet à 0
-		if (cur_header->flags >= 4 && cur_header->flags <= 7) {
+		if (ad->flags >= 4 && ad->flags <= 7) {
 			cur_header->flags -= 4;
 		}
 		//On change de Cache_Block_Header puis on incrémente le nombre de Cache_Block_Header visité
-		&cur_header += sizeof(struct Cache_Block_Header);
+		address = address + sizeof(struct Cache_Block_Header);
 		tmp++;
 	}
 	//On retourne le Cache_Error
@@ -118,7 +131,12 @@ Cache_Error Cache_Invalidate(struct Cache *pcache){
 
 //! Lecture  (à travers le cache).
 Cache_Error Cache_Read(struct Cache *pcache, int irfile, void *precord){
+	int x;
 
+	int fd = open(pcache->file, O_RDONLY);
+	fseek(pcache->fp, 0, irfile);
+	read(fd, &precord, sizeof(struct Cache_Block_Header));
+	
 }
 
 //! Écriture (à travers le cache).
